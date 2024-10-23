bdbab37e4d47ae8e23fd06250e2b1a938b0b7048
sjh1108 <ch6854@naver.com>
2024-10-23 17:23:07 +0900

    Day01_07

--- src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
+++ src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.jdbc.student.domain;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -8,11 +10,42 @@ public class Student {
     public enum GENDER{
         M,F
     }
+    @Getter
     private final String id;
+    @Getter
     private final String name;
+    @Getter
     private final GENDER gender;
+    @Getter
     private final Integer age;
+    @Getter
     private final LocalDateTime createdAt;
 
+    public Student(String id, String name, GENDER gender, Integer age){
+        this(id, name, gender, age, LocalDateTime.now());
+    }
     //todo#0 필요한 method가 있다면 추가합니다.
+    public Student(String id, String name, GENDER gender, Integer age, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id)
+                && Objects.equals(name, student.name)
+                && Objects.equals(gender, student.gender)
+                && Objects.equals(age, student.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, gender, age);
+    }
 }
